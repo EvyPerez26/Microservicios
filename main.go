4e7b40fb5a271f182f9bd1c1915c83dfbd0f7a0a
EvyPerez26 <142377209+EvyPerez26@users.noreply.github.com>
2023-09-01 05:06:45 +0000

    SE AGREGO Microservisios

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
-import ( 
-    "fmt" 
-    "html" 
-    "log" 
+
+import (
+    "fmt"
+    "html"
+    "log"
     "net/http"
-    ) 
-func main () {
-	http.HandleFunc("/Carpetas", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Bienvenidos al compartimiento")
+)
+
+func main() {
+
+    http.HandleFunc("/analisis", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hola! este es un Microservicio, te encuentras solicitando el path: %q", html.EscapeString(r.URL.Path))
     })
-    log.Fatal(http.ListenAndServe(":5001", nil))
+
+    http.HandleFunc("/sistemas", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Microservicio con Go y Docker By jsuhula!")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+    
 }
